b78ed0d89eaf0b26586aba1f0c2d206c3566c115
juanjayala <juan.j.ayala@outlook.com>
2021-11-10 12:46:27 -0500

    some functionality of query

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from operator import ne
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from datetime import datetime, time
 import csv
 import sys
 import pickle
@@ -17,10 +17,18 @@ class TimeSeries(db.Model):
     CountryRegion = db.Column(db.String(200), primary_key=True)
     date_recorded = db.Column(db.String(200), primary_key=True)
     quantity = db.Column(db.Integer)
-    case_type = db.Column(db.Integer, primary_key=True)
+    case_type = db.Column(db.String(200), primary_key=True)
 
     def __rep__(self):
         return "<Entry %r>" % self.firstname
+
+    # TO TEST
+    def __init__(self):
+        self.ProvinceState = ""
+        self.CountryRegion = "Mitski"
+        self.date_recorded = "01-01-2000"
+        self.quantity = 100
+        self.case_type = "time_series"
 
 
 @app.route("/time_series/<type>", methods=["POST", "GET"])
@@ -65,6 +73,62 @@ def index():
 @app.route("/time_series", methods=["POST", "GET"])
 def time_series_home():
         return render_template("time_series.html")
+
+@app.route("/time_series/<type>/query", methods=["POST", "GET"])
+def ts_query(type):
+    if request.method == "POST":
+        column = request.form['column']
+        places = request.form['places']
+        dates = request.form['dates']
+        data = [type]
+        # data = request.form['data'] (for daily report)
+
+        # now that we have all the input, return the requested data 
+        places_lst = places.split(",")
+        dates_lst = dates.split(",")
+        # data_lst = data.split(",") (for daily report)
+
+        #if csv
+        result = print_csv(column, places_lst, dates_lst, data)
+
+        if result == "ERROR":
+            return "There was an issue with the parameters used"
+            #redirect('/time_series/<type>/query')
+
+        #elif JSON:
+
+        return render_template('displayquery.html', result=result)
+    else:
+        # delete this ?
+        column = ""
+        places_lst = ["aa"]
+        dates_lst = []
+        return render_template('tsquery.html', type=type)
+""" 
+@app.route("/time_series/query/csv", methods=["POST", "GET"])
+def ts_csv():
+ """
+
+def print_csv(column, places_lst, dates_lst, data):
+    header=""
+    result = ""
+    if column == "ProvinceState":
+        header += "ProvinceState"
+            
+    elif column == "CountryRegion":
+        header += "CountryRegion"
+        #for place in places_lst:
+        #    TimeSeries.query.
+
+        curr = TimeSeries.query.filter_by(CountryRegion='Afghanistan').first()
+        return header + curr.CountryRegion + curr.case_type + curr.quantity
+
+    else:
+        return "ERROR"
+
+
+
+    return result
 
 
 if __name__ == "__main__":
